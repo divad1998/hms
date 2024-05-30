07b08d54df98fe2a2632445e59fdd59342bd68c0
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-05-30 14:53:42 +0100

    logout

--- src/main/java/com/ingryd/hms/service/PatientService.java
+++
@@ -1,9 +0,0 @@
-package com.ingryd.hms.service;
-
-import com.ingryd.hms.dto.RegisterPatientDto;
-import org.springframework.stereotype.Service;
-
-public interface PatientService {
-
-    RegisterPatientDto registerUser(RegisterPatientDto dto);
-}
