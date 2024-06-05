258dcf892ff4153c02c9091438fe61e8633752ed
Mariam Jelili <maryamjelili96@gmail.com>
2024-06-05 20:45:06 +0100

    update

--- src/main/java/com/ingryd/hms/controller/AuthController.java
+++ src/main/java/com/ingryd/hms/controller/AuthController.java
@@ -14,6 +14,7 @@ import com.ingryd.hms.dto.HospitalDTO;
 import com.ingryd.hms.entity.Hospital;
 import com.ingryd.hms.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.ingryd.hms.dto.LoginDTO;
@@ -28,6 +29,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
     @Autowired
     private final AuthService authService;
--- src/main/java/com/ingryd/hms/service/AuthService.java
+++ src/main/java/com/ingryd/hms/service/AuthService.java
@@ -21,6 +21,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -112,5 +113,14 @@ public class AuthService {
 
     public void logout(String authToken){
         jwtService.invalidateToken(authToken);
+    }
+
+    /**
+     * Fetches the authenticated user.
+     * @return the authenticated user
+     */
+    public User getAuthUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
     }
 }
