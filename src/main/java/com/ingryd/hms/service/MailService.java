a02105666ce8e42b75dd88ea064d93ecd8d99794
divad <divadchigozie@gmail.com>
2024-06-26 23:12:12 +0100

    new jar

--- src/main/java/com/ingryd/hms/service/MailService.java
+++ src/main/java/com/ingryd/hms/service/MailService.java
@@ -31,7 +31,7 @@ public class MailService {
             messageHelper.setTo(user.getEmail());
             messageHelper.setFrom("hospitalmanagementsystem1.0@gmail.com", "HealthGRYD");
             messageHelper.setSubject("Verify your email");
-            messageHelper.setText("Dear " + user.getFirstName() + "," + " kindly hit: " + "https://healthgryd.onrender.com/api/v1/auth/email_verification/" + token + " to verify your email.");
+            messageHelper.setText("Dear " + user.getFirstName() + "," + " kindly hit: " + "http://localhost:8080/api/v1/auth/email_verification/" + token + " to verify your email.");
 
             //send
             mailSender.send(messageHelper.getMimeMessage());
@@ -49,7 +49,7 @@ public class MailService {
             messageHelper.setSubject("Reset your password");
             messageHelper.setTo(user.getEmail());
             messageHelper.setFrom("hospitalmanagementsystem1.0@gmail.com", "HealthGRYD");
-            messageHelper.setText(String.format("Dear %s, \nKindly hit: %s to reset your password. \nIf you didn't request this code, you can ignore this email", user.getFirstName(), "https://healthgryd.onrender.com/api/v1/reset_password/" + token));
+            messageHelper.setText(String.format("Dear %s, \nKindly hit: %s to reset your password. \nIf you didn't request this code, you can ignore this email", user.getFirstName(), "http://localhost:8080/api/v1/reset_password/" + token));
             mailSender.send(messageHelper.getMimeMessage());
         } catch (Exception e){
             Logger logger = LoggerFactory.getLogger(this.getClass().getName());
@@ -64,7 +64,7 @@ public class MailService {
             messageHelper.setSubject("Reset your password");
             messageHelper.setTo(user.getEmail());
             messageHelper.setFrom("hospitalmanagementsystem1.0@gmail.com", "HealthGRYD");
-            messageHelper.setText(String.format("Dear %s, \nYour reset token is %s. Click kindly hit: %s to reset your password. \nIf you didn't request this code, you can safely ignore this email", user.getFirstName(), token, "https://healthgryd.onrender.com/api/v1/reset-password"));
+            messageHelper.setText(String.format("Dear %s, \nYour reset token is %s. Click kindly hit: %s to reset your password. \nIf you didn't request this code, you can safely ignore this email", user.getFirstName(), token, "http://localhost:8080/api/v1/reset-password"));
             mailSender.send(messageHelper.getMimeMessage());
         } catch (Exception e){
             Logger logger = LoggerFactory.getLogger(this.getClass().getName());
